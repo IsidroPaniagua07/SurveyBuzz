8f1ac0654e589d8cfc5cd43666933cbe3409536c
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-03 01:44:25 -0700

    removed user-agent, not the issue

--- pages/shop/masks/index.js
+++ pages/shop/masks/index.js
@@ -16,21 +16,9 @@ const index = ({ masks }) => {
 };
 
 export const getStaticProps = async () => {
-  let masks;
-  try {
-    const res = await fetch(`${server}/api/masks`, {
-      method: "GET",
-      headers: {
-        // update with your user-agent
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-        Accept: "application/json; charset=UTF-8",
-      },
-    });
-    masks = await res.json();
-  } catch (e) {
-    console.log(e);
-  }
+  const res = await fetch(`${server}/api/masks`);
+  const masks = await res.json();
+
   return {
     props: {
       masks,
--- pages/shop/shirts/index.js
+++ pages/shop/shirts/index.js
@@ -16,23 +16,8 @@ const index = ({ shirts }) => {
 };
 
 export const getStaticProps = async () => {
-  let shirts;
-  try {
-      const res = await fetch(`${server}/api/shirts`,
-      {
-        method: "GET",
-        headers: {
-          // update with your user-agent
-          "User-Agent":
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-          Accept: "application/json; charset=UTF-8",
-        },
-      }
-    );
-    shirts = await res.json();
-} catch (e) {
-    console.log(e);
-  }
+  const res = await fetch(`${server}/api/shirts`);
+  const shirts = await res.json();
 
   return {
     props: {
