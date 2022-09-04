b6857447ddeb68b1ec4cbb46900c395689dc1a87
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-03 21:40:06 -0700

    testing local apis

--- pages/shop/masks/[id].js
+++ pages/shop/masks/[id].js
@@ -19,17 +19,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async (context) => {
-  let lol
-  const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = "mongodb+srv://isidropaniagua:J4Ka8QRFffy87VOJ@cluster0.tab1zab.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  client.connect(err => {
-    const collection = client.db("CaudillsCrafts").collection("Products");
-    if(err)console.log(err)
-    console.log('open')
 
-    client.close();
-  });
   const id = context.params.id;
   const res = await fetch(`${server}/api/masks/${id}`);
   const mask = await res.json();
