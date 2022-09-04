2343596dd0fa01608b969149f107ef4fe0934b51
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-03 21:42:43 -0700

    fixed production server url again =(

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? 'http://localhost:3000' : 'caudills-crafts-gfw2nl1uf-isidropaniagua07.vercel.app'
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://caudills-crafts-gfw2nl1uf-isidropaniagua07.vercel.app";
