d2ea83d51ff600d56b8b044f9f3690c8c26ab68f
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-04 20:22:27 -0700

    fixing gSP

--- pages/shop/masks/[id].js
+++ pages/shop/masks/[id].js
@@ -2,7 +2,7 @@ import connectToDatabase from '../../../utils/mongodb'
 
 const Mask = ({ mask }) => {
 
-  return <div>{mask[0].name}</div>;
+  return <div>{mask.name}</div>;
 };
 
 export const getStaticPaths = async () => {
@@ -30,8 +30,8 @@ export async function getStaticProps(context) {
 
   const mask = await db
     .collection("Products")
-    .find({})
-    .toArray()
+    .find({_id:id})
+
 
   return {
     props: {
