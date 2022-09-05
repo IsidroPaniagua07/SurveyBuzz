42c3caf468dbd8d816f75b8ba41637ff3b7ecd25
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-04 19:06:12 -0700

    added props and paths to masks/id

--- pages/shop/masks/[id].js
+++ pages/shop/masks/[id].js
@@ -1,52 +1,43 @@
+import connectToDatabase from '../../../utils/mongodb'
 
 const Mask = ({ mask }) => {
-  return <div>asd</div>;
+  return <div>{mask.name}</div>;
 };
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`${server}/api/masks`);
-//   const masks = await res.json();
+export const getStaticPaths = async () => {
+  const { db } = await connectToDatabase();
 
-//   const paths = masks.map((mask) => {
-//     return {
-//       params: { id: mask.id.toString() },
-//     };
-//   });
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  const masks = await db
+    .collection("Products")
+    .find({type:'mask'})
+    .toArray();
 
-// export async function getStaticProps() {
-//   const { db } = await connectToDatabase();
+  const paths = masks.map((mask) => {
+    return {
+      params: { id: mask._id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-//   const masks = await db
-//     .collection("Products")
-//     .find({})
+export async function getStaticProps(context) {
+  const { db } = await connectToDatabase();
+  const id = context.params.id
 
-//     .toArray();
+  const mask = await db
+    .collection("Products")
+    .find({_id:id})
 
-//   return {
-//     props: {
-//       movies: JSON.parse(JSON.stringify(movies)),
-//     },
-//   };
-// }
+  return {
+    props: {
+      mask: JSON.parse(JSON.stringify(mask)),
+    },
+  };
+}
 
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-//   const res = await fetch(`${server}/api/masks/${id}`, {
-//     headers: {
-//       Accept: "application/json, text/plain, */*",
-//       "User-Agent": "*",
-//     },
-//   });
-//   const mask = JSON.stringify(res.data);
 
-//   return {
-//     props: { mask: mask },
-//   };
-// };
 
 export default Mask;
