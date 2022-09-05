d482d512aa52d1131e07e528c9eefd94fc14983f
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-04 17:02:27 -0700

    test

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,5 +1,3 @@
-import Product from "../../components/Card/Card";
-
 import Link from "next/link";
 import Card from "../../components/Card/Card";
 
@@ -11,31 +9,36 @@ const index = ({ categories }) => {
           Shop
         </div>
       </div>
-      {/* <div className="flex justify-center items-center w-full h-full font-Roboto text-xl">
+      <div className="flex justify-center items-center w-full h-full font-Roboto text-xl">
         <div className="flex flex-row h-fit w-3/4 text-left justify-center items-center gap-2">
           {categories.map((category) => (
             <div key={category.name}>
               <Card
                 name={category.name}
-                url={`/shop/${category.name.toLowerCase()}`}
+                url={`/shop/`}
               />
             </div>
-          ))} */}
-        {/* </div> */}
-      {/* </div> */}
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-// export const getStaticProps = async () => {
-//   const res = await fetch(`${server}/api/categories`);
-//   const categories = await res.json();
 
-//   return {
-//     props: {
-//       categories,
-//     },
-//   };
-// };
+export async function getStaticProps() {
+  const { db } = await connectToDatabase();
+
+  const masks = await db
+    .collection("Products")
+    .find({type: 'mask'})
+    .toArray();
+
+  return {
+    props: {
+      masks: JSON.parse(JSON.stringify(masks)),
+    },
+  };
+}
 
 export default index;
