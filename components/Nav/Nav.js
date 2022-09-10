0c760305a4e5773d2893e3253a73ba4f7c9a9d73
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-09 22:57:55 -0700

    search bar style

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/dist/client/link";
 
-
 const Nav = () => {
   return (
     <nav className="flex flex-row w-full h-[7%] px-4 justify-between items-center font-RobotoMono">
@@ -10,9 +9,14 @@ const Nav = () => {
           <a>Home</a>
         </Link>
       </div>
-      <form onSubmit={()=>{}} className="flex flex-row h-full w-1/3 justify-center items-center">
+      <form
+        onSubmit={() => {}}
+        className="flex flex-row h-full w-1/3 justify-center items-center"
+      >
         <input className=" flex h-8 w-[20rem] border border-gray-600 rounded rounded-r-none bg-white" />
-        <button className=" flex h-8 w-[4rem] border border-gray-600 border-l-0 bg-slate-800 text-white rounded rounded-l-none shadow-md justify-center items-center">Search</button>
+        <button className=" flex h-8 w-[4rem] border border-gray-600 border-l-0 bg-slate-600 text-white rounded rounded-l-none shadow-md justify-center items-center">
+          Search
+        </button>
       </form>
       <div className="flex flex-row gap-10 h-full w-1/3 justify-end items-center">
         <Link href="/shop">
