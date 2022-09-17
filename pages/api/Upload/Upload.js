4221a0206a56f45162dc99ffa92e793283efc1cc
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-16 20:47:41 -0700

    .

--- pages/api/Upload/Upload.js
+++ pages/api/Upload/Upload.js
@@ -2,16 +2,16 @@ import clientPromise from "../../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("nextjs-mongodb-demo");
+  const db = client.db("CaudillsCrafts");
   // switch (req.method) {
-    // case "POST":
-      // let bodyObject = JSON.parse(req.body);
-      // let myPost = await db.collection("posts").insertOne(bodyObject);
-      res.json({ status: 200, data: "" });
-      // break;
-    // case "GET":
-    //   const allPosts = await db.collection("allPosts").find({}).toArray();
-    //   res.json({ status: 200, data: allPosts });
-    //   break;
+  // case "POST":
+  // let bodyObject = JSON.parse(req.body);
+  // let myPost = await db.collection("posts").insertOne(bodyObject);
+  res.json({ status: 200, data: "" });
+  // break;
+  // case "GET":
+  //   const allPosts = await db.collection("allPosts").find({}).toArray();
+  //   res.json({ status: 200, data: allPosts });
+  //   break;
   // }
 }
