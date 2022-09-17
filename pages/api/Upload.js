61f4c99c8c592793a42e5b4f40058f067c1ca909
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-16 23:29:11 -0700

    testing api

--- pages/api/Upload.js
+++ pages/api/Upload.js
@@ -1,7 +1,7 @@
 import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
-  console.log(req.body)
+
   const client = await clientPromise;
   const db = client.db("CaudillsCrafts")
   const shirts = await db
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   // case "POST":
   // let bodyObject = JSON.parse(req.body);
   // let myPost = await db.collection("posts").insertOne(bodyObject);
-  res.json({ status: 200, data: shirts });
+  res.json({ status: 200, data: req.body });
   // break;
   // case "GET":
   //   const allPosts = await db.collection("allPosts").find({}).toArray();
