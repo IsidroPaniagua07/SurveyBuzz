71a528b27f0ff0d68f1abe22c96d21d644b3f850
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-17 00:51:02 -0700

    implementing api to upload image

--- pages/api/Upload.js
+++ pages/api/Upload.js
@@ -4,18 +4,19 @@ export default async function handler(req, res) {
   const {image} = req.body
   const client = await clientPromise;
   const db = client.db("CaudillsCrafts")
+  switch (req.method) {
+  case "POST":
   const shirts = await db
     .collection("Products")
     .create({ item: "test", qty: 15, image: image })
-  // switch (req.method) {
-  // case "POST":
+    break;
   // let bodyObject = JSON.parse(req.body);
   // let myPost = await db.collection("posts").insertOne(bodyObject);
   // res.json({ status: 200, data: req.body });
-  // break;
   // case "GET":
   //   const allPosts = await db.collection("allPosts").find({}).toArray();
   //   res.json({ status: 200, data: allPosts });
   //   break;
   // }
+  }
 }
