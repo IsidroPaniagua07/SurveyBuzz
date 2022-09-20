10b64cd97371be40abec2ee31eca273f8f17f7dd
Isidro Paniagua <Isidro.Paniagua@jamulcasinosd.com>
2022-09-19 17:55:17 -0700

    setting getStaticPros

--- pages/shop/shirts/index.js
+++ pages/shop/shirts/index.js
@@ -5,42 +5,42 @@ import Card from "../../../components/Card/Card";
 const index = ({ shirts }) => {
   return (
     <div className="flex flex-row gap-2">asd
-      {/* {shirts.map((shirt) => {
+      {shirts.map((shirt) => {
         return (
           <div key={shirt.name}>
             <Card name={shirt.name} url={`/shop/shirts/${shirt._id}`} />
           </div>
         );
-      })} */}
+      })}
     </div>
   );
 };
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
   // const { db } = await connectToDatabase();
 
-  const shirts = productData.shirts
+//   const shirts = productData.shirts
 
-
-  return {
-    props: {
-      shirts: shirts,
-    },
-  };
-}
-// export async function getStaticProps() {
-//   const { db } = await connectToDatabase();
-
-//   const shirts = await db
-//     .collection("Products")
-//     .find({ type: "shirt" })
-//     .toArray();
 
 //   return {
 //     props: {
-//       shirts: JSON.parse(JSON.stringify(shirts)),
+//       shirts: shirts,
 //     },
 //   };
 // }
+export async function getStaticProps() {
+  const { db } = await connectToDatabase();
+
+  const shirts = await db
+    .collection("Products")
+    .find({ type: "shirt" })
+    .toArray();
+
+  return {
+    props: {
+      shirts: JSON.parse(JSON.stringify(shirts)),
+    },
+  };
+}
 
 export default index;
