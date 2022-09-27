4f3ed1ddd318fcb83fc8d94c04aa6c19bde7bb1f
Isidro Paniagua <107605919+IsidroPaniagua07@users.noreply.github.com>
2022-09-26 19:08:32 -0700

    fixing upload

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 
 const index = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [data, setData] = useState({
-    name: '',
-    description: '',
-    category: '',
-    price: '',
-    image: ''
+  const [inputData, setInputData] = useState({
+    name: "",
+    description: "",
+    category: "mask",
+    price: "",
+    image: "",
   });
   let server;
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
@@ -27,14 +27,14 @@ const index = () => {
     }
     formData.append("upload_preset", "uploads");
 
-    const data = await fetch(
+    const imageData = await fetch(
       "https://api.cloudinary.com/v1_1/caudillscrafts/image/upload",
       {
         method: "POST",
         body: formData,
       }
     ).then((r) => r.json());
-    console.log(data.url);
+    console.log(imageData.url);
     let res = await fetch(
       // development build code
 
@@ -42,12 +42,11 @@ const index = () => {
       {
         method: "POST",
         body: JSON.stringify({
-          name: data.name,
-          description: data.description,
-          type: "",
-          price: data.price,
-          image: data.url,
-          
+          name: inputData.name,
+          description: inputData.description,
+          type: inputData.category,
+          price: inputData.price,
+          image: imageData.url,
         }),
       }
     ).then((r) => r.json());
@@ -55,41 +54,51 @@ const index = () => {
   };
 
   const handleOnChange = (e) => {
-    const {id, value} = e.target
-    setData({ ...data, [id]: value});
+    const { id, value } = e.target;
+    setInputData({ ...inputData, [id]: value });
   };
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    console.log(inputData);
+  }, [inputData]);
   return (
     <div className="flex h-full w-full  justify-center items-center">
       <form onSubmit={uploadImage} className="flex w-fit h-fit flex-col gap-4">
         <div className="flex justify-between">
           <label>Name:</label>
-          <input id='name'
+          <input
+            id="name"
             onChange={(e) => handleOnChange(e)}
             className="border border-black"
           />
         </div>
         <div className="flex justify-between">
           <label>Description:</label>
-          <input id='description'
-            onChange={(e) => handleOnChange(e)} className="border border-black" />
+          <input
+            id="description"
+            onChange={(e) => handleOnChange(e)}
+            className="border border-black"
+          />
         </div>
         <div className="flex justify-between">
           <label>Category:</label>
-          <select id='category' onChange={(e) => handleOnChange(e)} className="border border-black" >
-            <option value="mask">Mask</option>  
-            <option value="shirt">Shirt</option>  
+          <select
+            id="category"
+            onChange={(e) => handleOnChange(e)}
+            className="border border-black"
+          >
+            <option value="mask">Mask</option>
+            <option value="shirt">Shirt</option>
           </select>
         </div>
         <div className="flex justify-between">
           <label>Price:</label>
-          <input id='price'
+          <input
+            id="price"
             onChange={(e) => handleOnChange(e)}
-            className="border border-black" />
+            className="border border-black"
+          />
         </div>
         <div className="flex flex-col justify-between ">
           <label htmlFor="myfile">Upload a photo:</label>
