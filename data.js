64346d3b8e1931e14a764e2cb260cf3f67c2fb29
Isidro Paniagua <107605919+IsidroPaniagua07@users.noreply.github.com>
2022-09-26 19:18:07 -0700

    fixing post request

--- data.js
+++ data.js
@@ -13,17 +13,17 @@ const productData =
             {
                 _id: 1,
                 name: "Micky Mouse mask",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             },
             {
                 _id: 2,
                 name: "Anime mask",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             },
             {
                 _id: 3,
                 name: "Villians mask",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             }
 
         ],
@@ -32,17 +32,17 @@ const productData =
             {
                 _id: 4,
                 name: "Micky Mouse shirt",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             },
             {
                 _id: 5,
                 name: "Anime shirt",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             },
             {
                 _id: 6,
                 name: "Villians shirt",
-                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1663467409/My%20Uploads/vhsrip4xj3ebi82r6xrn.jpg"
+                image: "https://res.cloudinary.com/caudillscrafts/image/upload/v1664244731/My%20Uploads/n4jpjymgrwrlk8wfrygk.png"
             }
 
         ]
