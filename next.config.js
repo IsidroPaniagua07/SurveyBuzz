7a4c3ce8373d62e857ee0245ea11c8050116e6ff
Isidro Paniagua <107605919+IsidroPaniagua07@users.noreply.github.com>
2022-10-24 21:02:49 -0700

    adding api redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://surveybuzz.vercel.app/:path*'
+      }
+    ]
+  },
   swcMinify: true,
   images: {
     domains: ["res.cloudinary.com"],
